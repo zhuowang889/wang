dcef4ffb405a55554e60f9856e21603a9ab6c6a8
lijinlei <ljl86@qq.com>
2018-09-12 14:18:07 +0800

    add text

--- Apps/Api/Controller/IndexController.class.php
+++ Apps/Api/Controller/IndexController.class.php
@@ -92,20 +92,74 @@ class IndexController extends Controller {
         curl_setopt($ch, CURLOPT_HEADER, 0);
         $output = curl_exec($ch);
         curl_close($ch);
-        $object = simplexml_load_string($output);
-        $val = json_decode(json_encode(simplexml_load_string($output, 'SimpleXMLElement', LIBXML_NOCDATA)), true);
-        echo "<pre/>";
-        var_dump($val);die;
-        echo $object->Ad->Inline->Impression;
-        $rsJson = json_encode(\xml2array($object));
-        echo <<<EOT
-<script>
-var mystr = '{$rsJson}',
-myjson = JSON.parse(mystr);
-</script>
-EOT;
-        print_r($object);exit;
+        $object = simplexml_load_string($output, 'SimpleXMLElement', LIBXML_NOCDATA);
+        $object = $object->Ad->InLine;
+        $arr = json_decode(json_encode($object), true);
+        //print_r($arr);exit;
+        $rsArr = array(
+            'title' => $arr['AdTitle'],
+            'description' => $arr['Description'],
+            'impressionUrl' => $arr['Impression']['URL'],
+            'videoDuration' => $arr['Duration'],
+            'videoAdId' => $arr['AdId'],
+            'videoClickThroughUrl' => $arr['Video']['VideoClicks']['ClickThrough']['URL'],
+            'videoAttrDelivery' => $arr['Video']['MediaFiles']['MediaFile']['@attributes']['delivery'],
+            'videoAttrBitrate' => $arr['Video']['MediaFiles']['MediaFile']['@attributes']['bitrate'],
+            'videoAttrWidth' => $arr['Video']['MediaFiles']['MediaFile']['@attributes']['width'],
+            'videoAttrHeight' => $arr['Video']['MediaFiles']['MediaFile']['@attributes']['height'],
+            'videoAttrType' => $arr['Video']['MediaFiles']['MediaFile']['@attributes']['type'],
+            'videoUrl' => $arr['Video']['MediaFiles']['MediaFile']['URL'],
+            'trackingEventUrlStart' => $arr['TrackingEvents']['Tracking']['0']['URL'],
+            'trackingEventUrlMidpoint' => $arr['TrackingEvents']['Tracking']['1']['URL'],
+            'trackingEventUrlFirstQuartile' => $arr['TrackingEvents']['Tracking']['2']['URL'],
+            'trackingEventUrlThirdQuartile' => $arr['TrackingEvents']['Tracking']['3']['URL'],
+            'trackingEventUrlComplete' => $arr['TrackingEvents']['Tracking']['4']['URL'],
+            'trackingEventUrlMute' => $arr['TrackingEvents']['Tracking']['5']['URL'],
+            'trackingEventUrlPause' => $arr['TrackingEvents']['Tracking']['6']['URL'],
+            'trackingEventUrlReplay' => $arr['TrackingEvents']['Tracking']['7']['URL'],
+            'trackingEventUrlFullscreen' => $arr['TrackingEvents']['Tracking']['8']['URL'],
+            'trackingEventUrlStop' => $arr['TrackingEvents']['Tracking']['9']['URL'],
+            'trackingEventUrlUnmute' => $arr['TrackingEvents']['Tracking']['10']['URL'],
+            'trackingEventUrlResume' => $arr['TrackingEvents']['Tracking']['11']['URL'],
+        );
+        print_r($rsArr);exit;
+        $rsJson = json_encode($rsArr);
+        return $rsJson;
+    }
+    //接口请求广告
+    public function text()
+    {
+        $zones = 10;
+        $prefix = 'revive-0-';
+        $loc = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+        
+        //http://101.132.106.202/www/delivery/asyncspc.php?zones=10&prefix=revive-0-&loc=http%3A%2F%2Flocalhost%2Fdemo.html
+        $url = $this->adServerUrl.'/delivery/asyncspc.php?zones='.$zones.'&prefix='.$prefix.'&loc='.$loc;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        $output = curl_exec($ch);
+        echo $output;die;
+        curl_close($ch);
+        $arr = json_decode($output,true);
+        $html = $arr[$prefix.'0']['html'];
+        //echo $html;
+       // $matches = [];
+        
+/*         if(preg_match("/href='([^']+)'.+?src='([^']+)'.+?src='([^']+)/", $html, $matches)){
+            var_dump($matches);exit;
+            $arr[$prefix.'0']['url'] = $matches[1];
+            $arr[$prefix.'0']['text'] = $matches[2];
+            $arr[$prefix.'0']['showStatisticsUrl'] = $matches[3];
+            unset($arr[$prefix.'0']['html']);
+            $output = json_encode($arr);
+        }
+        print_r($arr); */
         return $output;
     }
+    /*
+     * //href='([^']+)'.+?>(.+?)<\\\a>.+?src='([^']+)'.+?src='([^']+)'
+     * */
     
 }
