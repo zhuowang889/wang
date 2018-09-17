1d3b6695935d56cd7a2e791f88f1f9d9ff9524f0
zhangcaijie <zhuchipeople@163.com>
2018-09-17 11:50:26 +0800

    完成了部分接口包括回调接口，回调接口还在完善中

--- Apps/Api/Controller/IndexController.class.php
+++ Apps/Api/Controller/IndexController.class.php
@@ -4,9 +4,12 @@ use Think\Controller;
 
 class IndexController extends Controller {
     public $adServerUrl = 'http://101.132.106.202/www';
-	//接口请求广告
+    
+	//接口请求图片广告
     public function index()
     {
+    	$zones = I('get.zoneId');
+    	//正式部署要删除掉具体参数
     	$zones = 7;
     	$prefix = 'revive-0-';
     	$loc = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
@@ -18,21 +21,22 @@ class IndexController extends Controller {
     	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     	curl_setopt($ch, CURLOPT_HEADER, 0);
     	$output = curl_exec($ch);
-    	//var_dump($output);die;
     	curl_close($ch);
     	$arr = json_decode($output,true);
     	$html = $arr[$prefix.'0']['html'];
-    	//echo $html;
     	$matches = [];
+    	$res = [];
     	if(preg_match("/href='([^']+)'.+?src='([^']+)'.+?src='([^']+)'/", $html, $matches)){
-    	    $arr[$prefix.'0']['url'] = $matches[1];
-    	    $arr[$prefix.'0']['img1'] = $matches[2];
-    	    $arr[$prefix.'0']['img2'] = $matches[3];
-    	    unset($arr[$prefix.'0']['html']);
-    	    $output = json_encode($arr);
+    	    $res['clickUrl'] = $matches[1];
+    	    $res['imgUrl'] = $matches[2];
+    	    $res['callBackParam'] = $matches[3];
+    	    $res['width'] = $arr[$prefix.'0']['width'];
+    	    $res['height'] = $arr[$prefix.'0']['height'];
     	}
-    	print_r($arr);
-    	return $output;
+    	/* echo "<pre/>";
+    	var_dump($res); */
+    	echo json_encode($res);
+    	return json_encode($res);
     }
     /**
      * DHTML广告
@@ -40,7 +44,10 @@ class IndexController extends Controller {
      */
     public function dhtmlAd()
     {
-        $params = array('zoneid'=>9,
+    	$zoneId = I('get.zoneId');
+    	//正式部署要删除掉具体参数
+    	$zoneId = 9;
+        $params = array('zoneid'=>$zoneId,
             'layerstyle'=>'geocities',
             'align'=>'right',
             'padding'=>2,
@@ -54,23 +61,22 @@ class IndexController extends Controller {
         curl_setopt($ch, CURLOPT_HEADER, 0);
         $output = curl_exec($ch);
         curl_close($ch);
-        //echo $output;
         $pattern = '/td\s+width=\\\"(?P<width>\d+)\\\"\s+height=\\\"(?P<height>\d+)';
         $pattern .= ".+?href=\\\'(?P<ckStatistics>[^']+)\\\'.+?src=\\\'(?P<img>[^']+)\\\'.+?src=\\\'(?P<showStatistics>[^']+)\\\'/";
-        //.+?src='([^']+)'
         $matches = [];
         if(preg_match($pattern, $output, $matches)){
-            $rs = array('width'=>$matches['width'],
-                'height'=>$matches['height'],
-                'ckStatisticsUrl'=>$matches['ckStatistics'],
-                'img'=>$matches['img'],
-                'showStatisticsUrl'=>$matches['showStatistics']
-            );
-            echo "<pre/>";
-            print_r($rs);
-            return $rs;
+            $rs = array(
+                		'clickUrl'=>$matches['ckStatistics'],
+                		'imgUrl'=>$matches['img'],
+               			'callBackParam'=>$matches['showStatistics'],
+            			'width'=>$matches['width'],
+            			'height'=>$matches['height']
+            	);
         }
-        return $output;
+        //echo "<pre/>";
+       // var_dump($rs);
+        //echo json_encode($rs);
+        return json_encode($rs);
     }
     /**
      * @desc 视频广告
@@ -78,11 +84,14 @@ class IndexController extends Controller {
      */
     public function video()
     {
+    	$zoneId = I('get.zoneId');
+    	//正式部署要删除掉具体参数
+    	$zoneId = 13;
         $params = array(
             'script'=>'bannerTypeHtml:vastInlineBannerTypeHtml:vastInlineHtml',
             'format'=>'vast',
             'nz'=>1,
-            'zones'=>'pre-roll=13'
+            'zones'=>"pre-roll=$zoneId"
         );
         $queryParams = http_build_query($params);
         $queryParams = str_replace('%3A', ':', $queryParams);
@@ -96,36 +105,34 @@ class IndexController extends Controller {
         $object = simplexml_load_string($output, 'SimpleXMLElement', LIBXML_NOCDATA);
         $object = $object->Ad->InLine;
         $arr = json_decode(json_encode($object), true);
-        //print_r($arr);exit;
         $rsArr = array(
-            'title' => $arr['AdTitle'],
-            'description' => $arr['Description'],
-            'impressionUrl' => $arr['Impression']['URL'],
-            'videoDuration' => $arr['Video']['Duration'],
-            //'videoAdId' => $arr['Video']['AdId'],
-            'videoClickThroughUrl' => $arr['Video']['VideoClicks']['ClickThrough']['URL'],
-            //'videoAttrDelivery' => $arr['Video']['MediaFiles']['MediaFile']['@attributes']['delivery'],
-            'videoAttrBitrate' => $arr['Video']['MediaFiles']['MediaFile']['@attributes']['bitrate'],
-            'videoAttrWidth' => $arr['Video']['MediaFiles']['MediaFile']['@attributes']['width'],
-            'videoAttrHeight' => $arr['Video']['MediaFiles']['MediaFile']['@attributes']['height'],
+            //'title' => $arr['AdTitle'],
+            //'description' => $arr['Description'],
+            'callBackParam' => $arr['Impression']['URL'],
+            //'videoDuration' => $arr['Video']['Duration'],
+            'clickUrl' => $arr['Video']['VideoClicks']['ClickThrough']['URL'],
+            //'bitrate' => $arr['Video']['MediaFiles']['MediaFile']['@attributes']['bitrate'],
+            'Width' => $arr['Video']['MediaFiles']['MediaFile']['@attributes']['width'],
+            'Height' => $arr['Video']['MediaFiles']['MediaFile']['@attributes']['height'],
             'videoAttrType' => $arr['Video']['MediaFiles']['MediaFile']['@attributes']['type'],
             'videoUrl' => $arr['Video']['MediaFiles']['MediaFile']['URL'],
-            'trackingEventUrlStart' => $arr['TrackingEvents']['Tracking']['0']['URL'],
-            'trackingEventUrlMidpoint' => $arr['TrackingEvents']['Tracking']['1']['URL'],
-            'trackingEventUrlFirstQuartile' => $arr['TrackingEvents']['Tracking']['2']['URL'],
-            'trackingEventUrlThirdQuartile' => $arr['TrackingEvents']['Tracking']['3']['URL'],
-            'trackingEventUrlComplete' => $arr['TrackingEvents']['Tracking']['4']['URL'],
-            'trackingEventUrlMute' => $arr['TrackingEvents']['Tracking']['5']['URL'],
-            'trackingEventUrlPause' => $arr['TrackingEvents']['Tracking']['6']['URL'],
-            'trackingEventUrlReplay' => $arr['TrackingEvents']['Tracking']['7']['URL'],
-            'trackingEventUrlFullscreen' => $arr['TrackingEvents']['Tracking']['8']['URL'],
-            'trackingEventUrlStop' => $arr['TrackingEvents']['Tracking']['9']['URL'],
-            'trackingEventUrlUnmute' => $arr['TrackingEvents']['Tracking']['10']['URL'],
-            'trackingEventUrlResume' => $arr['TrackingEvents']['Tracking']['11']['URL'],
+            //'trackingEventUrlStart' => $arr['TrackingEvents']['Tracking']['0']['URL'],
+            //'trackingEventUrlMidpoint' => $arr['TrackingEvents']['Tracking']['1']['URL'],
+            //'trackingEventUrlFirstQuartile' => $arr['TrackingEvents']['Tracking']['2']['URL'],
+            //'trackingEventUrlThirdQuartile' => $arr['TrackingEvents']['Tracking']['3']['URL'],
+            //'trackingEventUrlComplete' => $arr['TrackingEvents']['Tracking']['4']['URL'],
+            //'trackingEventUrlMute' => $arr['TrackingEvents']['Tracking']['5']['URL'],
+            //'trackingEventUrlPause' => $arr['TrackingEvents']['Tracking']['6']['URL'],
+            //'trackingEventUrlReplay' => $arr['TrackingEvents']['Tracking']['7']['URL'],
+            //'trackingEventUrlFullscreen' => $arr['TrackingEvents']['Tracking']['8']['URL'],
+            //'trackingEventUrlStop' => $arr['TrackingEvents']['Tracking']['9']['URL'],
+            //'trackingEventUrlUnmute' => $arr['TrackingEvents']['Tracking']['10']['URL'],
+            //'trackingEventUrlResume' => $arr['TrackingEvents']['Tracking']['11']['URL'],
         );
-        echo "<pre/>";
-        print_r($rsArr);exit;
+        //echo "<pre/>";
+        //print_r($rsArr);exit;
         $rsJson = json_encode($rsArr);
+        echo $rsJson;
         return $rsJson;
     }
     /**
@@ -133,14 +140,14 @@ class IndexController extends Controller {
      * @return json
      */
     public function suVideo(){
-    	$zone_id = I('get.zone_id');
+    	$zoneId = I('get.zoneId');
     	//正式部署要删除下面的变量
-    	$zone_id = 12;
+    	$zoneId = 12;
     	$params = array(
     			'script'=>'bannerTypeHtml:vastInlineBannerTypeHtml:vastInlineHtml',
     			'format'=>'vast',
     			'nz'=>1,
-    			'zones'=>'pre-roll='.$zone_id,
+    			'zones'=>'pre-roll='.$zoneId,
     	);
     	$queryParams = http_build_query($params);
     	$queryParams = str_replace('%3A', ':', $queryParams);
@@ -152,21 +159,23 @@ class IndexController extends Controller {
     	$output = curl_exec($ch);
     	curl_close($ch);
     	$val = json_decode(json_encode(simplexml_load_string($output, 'SimpleXMLElement', LIBXML_NOCDATA)), true);
-    	$arr['title'] = $val['Ad']['InLine']['AdTitle'];
-    	$arr['impression_url'] = $val['Ad']['InLine']['Impression']['URL'];
+    	//$arr['title'] = $val['Ad']['InLine']['AdTitle'];
+    	$arr['callBackParam'] = $val['Ad']['InLine']['Impression']['URL'];
     	$arr['adImg'] = $val['Ad']['InLine']['NonLinearAds']['NonLinear']['@attributes'];
-    	$arr['adImg_url'] = $val['Ad']['InLine']['NonLinearAds']['NonLinear']['URL'];
-    	$arr['click'] = $val['Ad']['InLine']['NonLinearAds']['NonLinear']['NonLinearClickThrough']['URL'];
+    	$arr['img_url'] = $val['Ad']['InLine']['NonLinearAds']['NonLinear']['URL'];
+    	$arr['clickUrl'] = $val['Ad']['InLine']['NonLinearAds']['NonLinear']['NonLinearClickThrough']['URL'];
+    	//echo "<pre/>";
+    	//var_dump(json_encode($arr));
     	return json_encode($arr);
     }
     //接口请求广告
     public function text()
     {
+    	$zones = I('get.zoneId');
         $zones = 10;
         $prefix = 'revive-0-';
         $loc = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
         
-        //http://101.132.106.202/www/delivery/asyncspc.php?zones=10&prefix=revive-0-&loc=http%3A%2F%2Flocalhost%2Fdemo.html
         $url = $this->adServerUrl.'/delivery/asyncspc.php?zones='.$zones.'&prefix='.$prefix.'&loc='.$loc;
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
@@ -176,19 +185,27 @@ class IndexController extends Controller {
         curl_close($ch);
         $arr = json_decode($output,true);
         $html = $arr[$prefix.'0']['html'];
-        //echo $html;
         $matches = [];
         
         if(preg_match("/href='(?P<ckStatistics>[^']+)'.+?>(?P<text>[^<]+).+?src='(?P<showStatistics>[^']+)/", $html, $matches)){
             $arr[$prefix.'0']['ckStatistics'] = $matches['ckStatistics'];
             $arr[$prefix.'0']['text'] = $matches['text'];
             $arr[$prefix.'0']['showStatisticsUrl'] = $matches['showStatistics'];
-            unset($arr[$prefix.'0']['html']);
-            $output = json_encode($arr);
         }
-        print_r($arr);
-        echo "<pre/>";
-        return $output;
+        $res = [];
+        $res['clikcUrl'] = $arr[$prefix.'0']['ckStatistics'] = $arr[$prefix.'0']['ckStatistics'];
+        $res['callBackParam'] = $arr[$prefix.'0']['showStatisticsUrl'];
+        $res['text'] = $arr[$prefix.'0']['text'];
+        $res['width'] = $arr[$prefix.'0']['width'];
+        $res['height'] = $arr[$prefix.'0']['height'];
+        //echo "<pre/>";
+        var_dump(json_encode($res));
+        return json_encode($res);
     }
-    
+    //回调接口
+    function callBack(){
+    	$param = I('post.callBackParam');
+    	$param = 'http://localhost:8000/www/delivery/lg.php?bannerid=2&campaignid=2&zoneid=2&loc=http%3A%2F%2Flocalhost%2Fdemo.html&cb=dcf03d82cc';
+    	echo "<img src='$param' width='0' height='0' alt='' style='width: 0px; height: 0px;'>";
+    }
 }
