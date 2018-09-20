06496195ce20eca2eed5e6026001b2831ea01b47
lijinlei <ljl86@qq.com>
2018-09-20 13:10:27 +0800

    整理 UserCenter api,add ckeckLogin

--- Apps/Api/Common/function.php
+++ Apps/Api/Common/function.php
@@ -2,15 +2,22 @@
 
 /**
  * API 返回，0失败，1成功
- * @param number $code
+ * @param int|array $code_data
  * @param string $msg
  * @param string $referer
  */
-function res($code = 0, $msg = '失败', $referer = ''){
+function res($code_data, $msg = '', $referer = ''){
+    if(is_numeric($code_data)){
+        $code = $code_data;
+    }else{
+        list($code, $data) = $code_data;
+    }
     if($referer){
         header("Location:{$referer}");
     }
-    $res = array('code'=>$code, 'msg'=>$msg);
+    $res = array('code'=>$code);
+    isset($data) && $res['data'] = $data;
+    $res['msg'] = $msg;
     echo json_encode($res,JSON_UNESCAPED_UNICODE);
     exit;
 }
--- Apps/Api/Controller/UserCenterController.class.php
+++ Apps/Api/Controller/UserCenterController.class.php
@@ -1,13 +1,15 @@
 <?php
 namespace Api\Controller;
 use Think\Controller;
-use Think\Think;
-use Api\Model;
 use Api\Model\AdminModel;
 /*
  * @desc 用户中心API
  */
 class UserCenterController extends Controller {
+    private $adminModel;
+    protected function _initialize(){
+        $this->adminModel = new AdminModel();
+    }
     
 	/**
 	 * sso登陆接口
@@ -15,22 +17,55 @@ class UserCenterController extends Controller {
     public function index()
     {
         //模拟数据，待删除
-        $_POST = ['user_name'=>'admin', 'password'=>'123456'];
-    	$refer = I('refer', '', 'trim');
-    	$adminModel = new AdminModel();
-    	if($uid = $adminModel->checkLogin()){
-    	    $userInfo = $adminModel->getUserinfo($uid);
-    	    if($userInfo['status']==2){
+        $_POST = ['user_name'=>'admin', 'password'=>'123456', 'referer'=>''];
+    	$referer = I('post.referer', '', 'trim');
+    	if($uid = $this->adminModel->checkPassword()){
+    	    $status = $this->adminModel->getUserinfo($uid, 'status');
+    	    if($status == 2){
     	        res(0, '用户不允许登陆');
     	    }
-    	    if($adminModel->setUserSSO($uid)){
-    	        res(1, '登陆成功', $refer);
+    	    if($key = $this->adminModel->setUserSSO($uid)){
+    	        $referer && $referer .= (strpos($referer, '?')===false ? '?' : '&').'u='.$key;
+    	        res(1, '登陆成功', $referer);
     	    }
+    	    res(0, '登陆失败');
     	}else{
     	    res(0, '用户名或密码错误');
     	}
-    	res();
+    }
+    
+    /**
+     * user 获取用户信息
+     */
+    public function userinfo() {
+        //模拟数据，待删除
+        $_POST = ['id'=>1, 'u'=>'a7d0e7a977'];
+        if($SSOkey = I('post.u', '', 'trim')){
+            $uid = $this->adminModel->getUidFromSSOkey($SSOkey);
+        }else{
+            $uid = I('post.id', 0, 'intval');
+        }
+        
+        if($userInfo = $this->adminModel->getUserinfo($uid)){
+           res([1, $userInfo]); 
+        }else{
+           res(0, '不存在此用户或未登录'); 
+        }
+    }
+    
+    /**
+     * user 检查是否登陆
+     */
+    public function checkLogin() {
+        //模拟数据，待删除
+        $_POST = ['id'=>1];
+        $ssoKey = I('post.u', '', 'trim');
+        $uid = I('post.id', 0, 'intval');
+        if($uInfo = $this->adminModel->checkLogin($ssoKey, $uid)){
+            res([1,$uInfo], '已经登陆');
+        }else{
+            res(0, '未登录或过期');
+        }
     }
 
-   
 }
